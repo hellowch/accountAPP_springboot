0d25b61cc3af4f108db7e30271bb31b35768fecb
魏陈昊 <525691465@qq.com>
2020-10-30 11:15:30 +0800

    项目创建

---
+++ src/main/java/com/example/finalproject_springboot/FinalprojectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalproject_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalproject_springboot/FinalprojectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalproject_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
